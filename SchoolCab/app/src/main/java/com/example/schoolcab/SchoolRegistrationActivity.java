e0bf6d9be2cf1027d682799d7db33dcd1e5476b6
Charvi-Sharma <csharma71c@gmail.com>
2023-08-18 19:24:34 +0530

    Added Password Confirmation

--- SchoolCab/app/src/main/java/com/example/schoolcab/SchoolRegistrationActivity.java
+++ SchoolCab/app/src/main/java/com/example/schoolcab/SchoolRegistrationActivity.java
@@ -13,6 +13,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SchoolRegistrationActivity extends AppCompatActivity {
 
     private FirebaseFirestore db;
@@ -31,33 +34,44 @@ public class SchoolRegistrationActivity extends AppCompatActivity {
             EditText boardEditText = findViewById(R.id.school_board);
             EditText schoolIdEditText = findViewById(R.id.school_id);
             EditText passwordEditText = findViewById(R.id.school_password);
+            EditText confirmPasswordEditText = findViewById(R.id.confirm_password);
 
             String name = nameEditText.getText().toString();
             String board = boardEditText.getText().toString();
             String schoolId = schoolIdEditText.getText().toString();
             String password = passwordEditText.getText().toString();
+            String confirmPassword = confirmPasswordEditText.getText().toString();
 
             Log.d("SchoolRegistration", "Name: " + name + ", Password: " + password +
-                                                "Board: " + board + "schoolId: " + schoolId);
-
-            School school = new School();
-            school.setName(name);
-            school.setSchoolId(schoolId);
-            school.setBoard(board);
-            school.setVerifiedStatus(false);
-            school.setPassword(password);
+                                                "Board: " + board + "schoolId: " + schoolId + "confirmPassword: " + confirmPassword);
 
 
-            // Add school to Firestore
-            db.collection("schools")
-            .add(school)
-                    .addOnSuccessListener(documentReference -> {
-                        Toast.makeText(this, "School registered successfully!", Toast.LENGTH_SHORT).show();
-                    })
-                    .addOnFailureListener(e -> {
-                        Log.e("SchoolRegistration", "Error registering school", e);
-                        Toast.makeText(this, "Error registering school", Toast.LENGTH_SHORT).show();
-                    });
+            Matcher matcher = Pattern.compile(password).matcher(confirmPassword);
+            if (matcher.matches()) {
+                Log.e("Matched Passwords", password);
+                School school = new School();
+                school.setName(name);
+                school.setSchoolId(schoolId);
+                school.setBoard(board);
+                school.setVerifiedStatus(false);
+                school.setPassword(password);
+
+
+                // Add school to Firestore
+                db.collection("schools")
+                        .add(school)
+                        .addOnSuccessListener(documentReference -> {
+                            Toast.makeText(this, "School registered successfully!", Toast.LENGTH_SHORT).show();
+                        })
+                        .addOnFailureListener(e -> {
+                            Log.e("SchoolRegistration", "Error registering school", e);
+                            Toast.makeText(this, "Error registering school", Toast.LENGTH_SHORT).show();
+                        });
+
+            } else {
+                Toast.makeText(SchoolRegistrationActivity.this, "Passwords not Match", Toast.LENGTH_LONG).show();
+            }
+
         });
     }
 }
