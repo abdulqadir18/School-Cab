9f59b5b3896c7b7d020babaf7ff5f6d031304d76
Charvi-Sharma <csharma71c@gmail.com>
2023-09-17 15:29:03 +0530

    Added Report Input

--- SchoolCab/app/src/main/java/com/example/schoolcab/SchoolLoginActivity.java
+++ SchoolCab/app/src/main/java/com/example/schoolcab/SchoolLoginActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
