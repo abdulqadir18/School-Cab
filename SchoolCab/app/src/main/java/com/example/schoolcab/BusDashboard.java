c7ed0c2e8828db1be1ff973d4340795ec6c85dda
Charvi-Sharma <csharma71c@gmail.com>
2023-09-18 20:39:03 +0530

    Updated UI for Bus and Coordinator

--- SchoolCab/app/src/main/java/com/example/schoolcab/BusDashboard.java
+++ SchoolCab/app/src/main/java/com/example/schoolcab/BusDashboard.java
@@ -2,23 +2,57 @@ package com.example.schoolcab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 public class BusDashboard extends AppCompatActivity {
+
+    public static final String SHARED_PREFS = "shared_prefs";
+
+    // key for schoolId
+    public static final String sId = "sId";
+    // variable for shared preferences.
+    SharedPreferences sharedpreferences;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null){
+            Intent intent = new Intent(BusDashboard.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus_dashboard2);
 
-        Button attendance = findViewById(R.id.attendance);
-        Button busTracking = findViewById(R.id.busTracking);
-        Button logoutButton = findViewById(R.id.logout_button1);
-        Button notification = findViewById(R.id.notification1);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+//        Getting the school id saved in local preferences
+        sharedpreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        String schoolID = sharedpreferences.getString("sId", null);
+
+        RelativeLayout attendance = findViewById(R.id.attendance_button);
+        RelativeLayout busTracking = findViewById(R.id.bus_button);
+        RelativeLayout logoutButton = findViewById(R.id.logout_button);
+        RelativeLayout notification = findViewById(R.id.notification_button);
 
         attendance.setOnClickListener(v -> {
             Intent intent = new Intent(BusDashboard.this, AttendanceAddActivity.class);
@@ -26,9 +60,16 @@ public class BusDashboard extends AppCompatActivity {
 
         });
         logoutButton.setOnClickListener(v -> {
+            mAuth.signOut();
+
+//            removing the schooldid from shared preferences on logout
+            SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.remove(sId);
+            editor.remove("busId");
+            editor.apply();
+
             Intent intent = new Intent(BusDashboard.this, MainActivity.class);
             startActivity(intent);
-            Toast.makeText(BusDashboard.this, "Logged Out Succesfully", Toast.LENGTH_LONG).show();
         });
 
         busTracking.setOnClickListener(v -> {
